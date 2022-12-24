e53e58b6453a16a2136bc186f4e66e35fa309676
yousef <yousef@yousef.com>
2022-12-24 21:03:49 +0330

    add first project successfully

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField()
     status = models.BooleanField(default=False)
     content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True)
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
