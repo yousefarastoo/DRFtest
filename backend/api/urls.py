f58a1d919a36e2fd3c93770ff226fea2efc8f10b
yousef <yousef@yousef.com>
2022-12-24 21:36:14 +0330

    modified api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ArticleList
+from .views import ArticleList,ArticleCreateView
 
 app_name="api"
 
 urlpatterns = [
     path('', ArticleList.as_view(),name="list"),
+    path('create', ArticleCreateView.as_view(),name="create"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from blog.models import Article
 from .serializers import ArticleSerialiser
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView,ListCreateAPIView
 # Create your views here.
 
 class ArticleList(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerialiser
+
+
+class ArticleCreateView(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerialiser
